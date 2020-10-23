78794f6102443162f63746fff80144f19ec5f784
Charbel Atallah <chappa_atallah@hotmail.com>
2020-10-23 10:58:57 +0200

    Object orienterat

---
+++ src/Bil1.java
@@ -0,0 +1,7 @@
+public class Bil1 {
+    String regNr = "LOL666";
+    String märke = "Lamborghini";
+    int årsmodell = 2021;
+    int tjänstevikt = 1300;
+    int motoreffekt = 666;
+}
---
+++ src/Bil2.java
@@ -0,0 +1,7 @@
+public class Bil2 {
+    String regNr = "hej123";
+    String märke = "BMW";
+    int årsmodell = 2019;
+    int tjänstevikt = 1550;
+    int motoreffekt = 237;
+}
