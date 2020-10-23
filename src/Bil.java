3935cee702a68cef50171640600b278c129e9253
Charbel Atallah <chappa_atallah@hotmail.com>
2020-10-23 10:57:36 +0200

    Object orienterat

--- src/Bil.java
+++ src/Bil.java
@@ -1,4 +1,8 @@
+
+
+//Deklareraen referensvariabel (klassnamn namn;)
 public class Bil {
+    //Instansvariabler (Egenskaper)
     String regNr = "YHE 380";
     String märke = "Audi A3 Sportback";
     int årsmodell = 2017;
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        Bil c = new Bil();
+        Bil bil = new Bil(); //Skapar ett objekt med nyckelnamnet new
+        Bil bil1 = new Bil();
+        Bil bil2 = new Bil();
 
-        System.out.println(c.motoreffekt);
-        System.out.println(c.märke);
-        System.out.println(c.regNr);
-        System.out.println(c.tjänstevikt);
-        System.out.println(c.årsmodell);
+        System.out.println("Bil nummer 1: ");
+        System.out.println("Antalet hästkrafter: " + bil.motoreffekt);
+        System.out.println("Slags märke: " + bil.märke);
+        System.out.println("Registreringsnummer: " + bil.regNr);
+        System.out.println("Bilens tjänstevikt: " + bil.tjänstevikt);
+        System.out.println("Bilens årsmodell: " + bil.årsmodell);
+        System.out.println("------------------------------------------");
+
+        System.out.println("Bil nummer 2: ");
+        System.out.println("Slags märke: " + bil1.märke);
+        System.out.println("Registreringsnummer: " + bil1.regNr);
+        System.out.println("Bilens årsmodell: " + bil1.årsmodell);
+        System.out.println("Antalet hästkrafter: " + bil1.motoreffekt);
+        System.out.println("Bilens tjänstevikt: " + bil1.tjänstevikt);
+        System.out.println("------------------------------------------");
+
+        System.out.println("Bil nummer 3: ");
+        System.out.println("Slags märke: " + bil2.märke);
+        System.out.println("Registreringsnummer: " + bil2.regNr);
+        System.out.println("Bilens årsmodell: " + bil2.årsmodell);
+        System.out.println("Antalet hästkrafter: " + bil2.motoreffekt);
+        System.out.println("Bilens tjänstevikt: " + bil2.tjänstevikt);
+
     }
 }
