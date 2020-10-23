0aefd3498690a59019ceee4e2503b8df78ae117e
Charbel Atallah <chappa_atallah@hotmail.com>
2020-10-23 09:30:36 +0200

    Object orienterat

---
+++ src/Bil.java
@@ -0,0 +1,7 @@
+public class Bil {
+    String regNr = "YHE 380";
+    String märke = "Audi A3 Sportback";
+    int årsmodell = 2017;
+    int tjänstevikt = 1255;
+    int motoreffekt = 116;
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Bil c = new Bil();
+
+        System.out.println(c.motoreffekt);
+        System.out.println(c.märke);
+        System.out.println(c.regNr);
+        System.out.println(c.tjänstevikt);
+        System.out.println(c.årsmodell);
+    }
+}
